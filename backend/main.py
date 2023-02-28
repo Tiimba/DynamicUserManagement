f9cd07670dac5a40e3d55d704e6eb1e3078e2b2a
Timba <thiagodesouza07t@gmail.com>
2023-02-28 17:12:34 -0300

    Doc the routes

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from servers import Unix
 from models import User
+import configparser
 
 app = FastAPI()
-
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8000",
-# ]
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,40 +14,51 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+credentials = {
+    "connection_type": "password",
+    "username": "timba",
+    "password": "mudar123",
+    "ssh_pass": None
+}
+
+def get_unix_server(hostname):
+    return Unix(hostname=hostname, credentials=credentials)
+
 @app.get("/")
 async def root():
+    """
+    Página inicial da API
+    """
     return {"detail": "Boa sorte para usar! Feito com amor"}
 
 
 @app.get("/{hostname}/users")
 async def users(hostname):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    """
+    Obtém todos os usuários do servidor Unix
+    """
+    server = get_unix_server(hostname)
     return {"detail": server.get_users()}
 
 
 @app.get("/{hostname}/os")
 async def os(hostname):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    """
+    Obtém o sistema operacional do servidor Unix
+    """
+    server = get_unix_server(hostname)
     return {"detail": {"os": f"{server.get_os()}"}}
 
 
 @app.get("/{hostname}/groups")
 async def groups(hostname):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    server = get_unix_server(hostname)
     return {"detail": {"groups": server.get_groups()}}
 
 
 @app.post("/{hostname}/user/create")
 async def create_user(hostname, user: User):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    server = get_unix_server(hostname)
     user_dict = user.dict()
     return {"detail": server.create_user(username=user_dict["username"], password=user_dict["password"],
                                          groups=user_dict["groups"],
@@ -63,23 +68,17 @@ async def create_user(hostname, user: User):
 
 @app.get("/{hostname}/user/unlock/{username}")
 async def unlock_user(hostname, username):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    server = get_unix_server(hostname)
     return {"detail": server.unlock_user(user=username)}
 
 
 @app.get("/{hostname}/user/lock/{username}")
 async def unlock_user(hostname, username):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    server = get_unix_server(hostname)
     return {"detail": server.lock_user(user=username)}
 
 
 @app.delete("/{hostname}/user/{user}")
 async def delete_user(hostname, user):
-    server = Unix(hostname=hostname,
-                  credentials={"connection_type": "password", "username": "timba", "password": "mudar123",
-                               "ssh_pass": None})
+    server = get_unix_server(hostname)
     return {"detail": server.delete_user(user)}
